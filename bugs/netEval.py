68ba131a1f714c0740e5daf5e975ebad164cffb4
amapy-00 <s233514@dtu.dk>
2025-04-14 19:48:05 +0200

    fix sampleNetwork and netEval

--- bugs/netEval.py
+++ bugs/netEval.py
@@ -9,10 +9,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import glob
-import skimage.io
+import skimage.io as io
+import cv2
 import time
+from sklearn.model_selection import train_test_split
 
 from bugs.sampleNetwork import netFunc
+
+def one_hot_encode(targets, num_classes=None):
+    """
+    One-hot encode the target values.
+
+    Args:
+        targets (numpy.ndarray): Array of target values (integers).
+        num_classes (int): Total number of classes. If None, inferred from the targets.
+
+    Returns:
+        numpy.ndarray: One-hot encoded matrix of shape (num_samples, num_classes).
+    """
+    if num_classes is None:
+        num_classes = np.max(targets) + 1  # Infer number of classes from the targets
+    one_hot = np.zeros((len(targets), num_classes))
+    one_hot[np.arange(len(targets)), targets] = 1
+    return one_hot
+
+def resize_images_to_vectors(images):
+    num_images, height, width = images.shape
+    return images.reshape(num_images, height * width)
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in sorted(os.listdir(folder)):
+        img = io.imread(os.path.join(folder, filename))
+        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img_gray = img_gray / 255.0  # Normalize pixel values to [0, 1]
+        images.append(img_gray)
+
+    return np.array(images)
+
 
 def netEval(netFunc, dataPath, targetPath):
     '''
@@ -42,26 +76,26 @@ def netEval(netFunc, dataPath, targetPath):
     ext = os.path.splitext(targetPath)[-1]
     assert ext == '.txt' or ext == '.npy', f"Target path extension file {ext} is not supported, use .txt or .npy"
     
-    if ext == '.txt':
-        with open(targetPath) as f:
-            targetList = np.array(f.readlines()).astype(int)
-    else:
-        targetList = np.load(targetPath)
-    
-    
-    # Read in the images
-    imgList = glob.glob(dataPath+'/*.png')
-    assert imgList, f"No .png images found in folder {targetPath}"
-    imgsArr = np.array([skimage.io.imread(fname) for fname in imgList])
-    
+    targets = np.loadtxt(targetPath, dtype=int)
+    one_hot_targets = one_hot_encode(targets)
+
+    X_images = load_images_from_folder(dataPath)
+    X_vectors = resize_images_to_vectors(X_images)
+
+    # Split the dataset into training and testing sets
+    # take only test set
+    _, X_test, _, y_test = train_test_split(X_vectors, one_hot_targets, test_size=0.2, random_state=42)
+    X_test = X_test.T
+    y_test = np.argmax(y_test.T, axis=0)
+
     # Execute network
     t0 = time.time()
-    predList = netFunc(imgsArr)
+    predList = netFunc(X_test)
     t1 = time.time()
     
     # Calculate accuracy and execution time
-    accuracy = np.sum(np.equal(predList,targetList))/len(targetList)
-    execTime = t1-t0
+    accuracy = np.sum(np.equal(predList,y_test))/len(y_test)
+    execTime = t1-t0    
     
     return accuracy, execTime
         
@@ -70,7 +104,7 @@ def netEval(netFunc, dataPath, targetPath):
 if __name__ == "__main__":
     
     targetPath = '/home/amapy/Desktop/advanced_image_analysis_2025/Advanced-Image-Analysis/bugs/data/week9_BugNIST2D_train/train_targets.txt'
-    dataPath = '/home/amapy/Desktop/advanced_image_analysis_2025/Advanced-Image-Analysis/bugs/data/week9_BugNIST2D_train/train' 
+    dataPath = '/home/amapy/Desktop/advanced_image_analysis_2025/Advanced-Image-Analysis/bugs/data/week9_BugNIST2D_train/train/' 
     
     accuracy, execTime = netEval(netFunc, dataPath, targetPath)
     
